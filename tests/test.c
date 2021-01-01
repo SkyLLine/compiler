1668659b695774dc0039822458406cd64c628aed
SkyLLine <978586497@qq.com>
2021-01-01 13:29:29 +0800

    func done

--- tests/test.c
+++ tests/test.c
@@ -1,41 +1,12 @@
-/*
-I'm level 1 test.
-*/
-
-
-void good(int aa, int bb)
-{
-    printf("%c, %d\n", aa, bb);
-    int d = bb;
+int a=10;
+int main(){
+    int a;
+    a=1;
+    int i;
+    for( i=0;i<10;i=i+1){
+        int a=i*10+i%3;
+        printf("Test %d %d\n",i,a);
+    }
+    printf("Test %d\n",a);
     return 0;
 }
-
-int main() {
-int s;
-int c;
-int a[5][5];
-a[0][1] = 10;
-a[3][1] = 5;
-c = a[1][1];
-s = a[2][3];
-char ch;
-scanf("%c", ch);
-int b = 10;
-printf("%c\n", ch);
-// printf("result is: %d\n", a.a[0]);
-while(a[0][1]>0 && (s < 20 || s % 100 == 99)) {
-if(a[0][1] > 0){
-    printf("good! then\n");
-}
-a[0][1] = a[0][1] - 1;
-printf("result is: %d\n", a[0][1]);
-// good(1, 2);
-int i = a[1][2];
-int b = 10;
-for(i=0; i<b; i = i + 1) {
-printf("Have fun: %d\n", i);
-}
-}
-return 0;
-}
-// No more compilation error.
